615d961ab0c050512dff8ed6ef8ee815342e3edf
unknown <savonikmaksim@gmail.com>
2022-10-10 00:50:49 +0300

    v1.0

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import * as flsFunctions from "./modules/functions.js";
+import * as lazyLoad from "./modules/lazyload.js";
+import * as swiper from "./modules/swiper.js";
+
+flsFunctions.isWebp();
+lazyLoad.lazyLoad();
+swiper.swiper();
---
+++ src/js/modules/swiper.js
@@ -0,0 +1,18 @@
+export function swiper() {
+    const swiper = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+    });
+    const swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+            swiper: swiper,
+        },
+    });
+}
