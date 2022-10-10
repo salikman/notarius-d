ddfd97b517805a9905616db72be312e508a77e8d
unknown <savonikmaksim@gmail.com>
2022-10-10 16:16:41 +0300

    v1.1

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,24 @@ import * as swiper from "./modules/swiper.js";
 flsFunctions.isWebp();
 lazyLoad.lazyLoad();
 swiper.swiper();
+
+$(function() {
+	//BEGIN
+	$(".accordion__title").on("click", function(e) {
+
+		e.preventDefault();
+		var $this = $(this);
+
+		if (!$this.hasClass("accordion-active")) {
+			$(".accordion__content").slideUp(400);
+			$(".accordion__title").removeClass("accordion-active");
+			$('.accordion__arrow').removeClass('accordion__rotate');
+		}
+
+		$this.toggleClass("accordion-active");
+		$this.next().slideToggle();
+		$('.accordion__arrow',this).toggleClass('accordion__rotate');
+	});
+	//END
+	
+});
--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,11 +1,11 @@
 export function swiper() {
-    const swiper = new Swiper(".mySwiper", {
+    const swiper = new Swiper(".thumbs-slider", {
         spaceBetween: 10,
         slidesPerView: 4,
         freeMode: true,
         watchSlidesProgress: true,
     });
-    const swiper2 = new Swiper(".mySwiper2", {
+    const swiper2 = new Swiper(".slide-one", {
         spaceBetween: 10,
         navigation: {
             nextEl: ".swiper-button-next",
@@ -13,6 +13,32 @@ export function swiper() {
         },
         thumbs: {
             swiper: swiper,
+        },
+    });
+    const reviewsSlider = new Swiper(".reviews-slider", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+    const certificate = new Swiper(".certificate-slider", {
+        loop: true, // <- causes issues combined with multi row setup!!
+        slidesPerView: 3,
+        slidesPerColumn: 3,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
         },
     });
 }
